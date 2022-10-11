efd6c28a9e28c2176f2c3716c25da2ea5015aa9a
Doug Sellers <DouglasSSellers@gmail.com>
2022-10-10 21:59:49 -0400

    added branch

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,8 +8,13 @@ class PostList extends React.Component {
     this.props.fetchPosts();
   }
   render() {
-    return <div>PostList</div>;
+    console.log(this.props.posts);
+    return <div>Post List</div>;
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+  return { post: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
