e3e96b584f272d83f02d2a65130c40b7f0ecbc92
Doug Sellers <DouglasSSellers@gmail.com>
2022-10-13 18:50:28 -0400

    added details to PostDetails component

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -2,7 +2,11 @@ import React from "react";
 
 class PostDetails extends React.Component {
   render() {
-    return <div>Post Details</div>;
+    return (
+      <div>
+        <h1>Post Details</h1>s
+      </div>
+    );
   }
 }
 
